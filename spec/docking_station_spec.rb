e85bc77dc002b255cbc45a893161b2cf0d7122f1
muge <muge.hasilci@hotmail.com>
2018-10-03 16:57:25 +0100

    test for method working? and pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,8 @@ describe DockingStation do
   it "reponds to releasing bike" do
     expect(subject).to respond_to(:release_bike)
   end
-
+  it 'releases the working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
